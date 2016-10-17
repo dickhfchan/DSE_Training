4a4f47f9df48ac859b67e12613e7131f1a8d15bb
dickhfchan <dickhfchan@timingpower.com>
2016-10-17 15:37:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ sudo apt-get update
 
 https://docs.datastax.com/en/cassandra/2.0/cassandra/install/installDeb_t.html
 
+
+## Modify the cassandra.yaml configuration file before starting Cassandra cluster
+## listen address and rpc address should be the ip_addreess of host while the seeds are the ip address of any two of three nodes.
+# vi /etc/dse/cassandra/cassandra.yaml
+# listen_address: 192.168.3.100
+# rpc_address: 192.168.3.100
+# - seeds: "192.168.3.100,192.168.3.100"
+
