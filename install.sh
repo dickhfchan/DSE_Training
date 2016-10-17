27d04248d4909d39483f176961261dca1d0d77db
dickhfchan <dickhfchan@timingpower.com>
2016-10-17 14:50:55 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#install Oracle JDK
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+#install Cassandra Open Source Edition
+
+https://docs.datastax.com/en/cassandra/2.0/cassandra/install/installDeb_t.html
+
